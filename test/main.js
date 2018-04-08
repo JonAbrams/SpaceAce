a62967c4927c12547b19a798bfe3652dc9842d22
Jon Abrams <jon@jonabrams.com>
2018-04-08 13:27:52 -0700

    Custom actions

--- test/main.js
+++ test/main.js
@@ -64,18 +64,75 @@ describe('Space', function() {
     assert.equal(this.space.toString(), JSON.stringify(this.initialState));
   });
 
-  describe('updating', function() {
-    it('updates + adds single keys with a value', function() {
-      var space = this.space;
-      assert.strictEqual(typeof space('limit'), 'function');
-      space('limit')(6);
-      // existing space doesn't change
-      assert.strictEqual(space, this.space);
-      // correct values given to subscribers
-      assert.notStrictEqual(this.newSpace, space);
-      assert.strictEqual(this.newSpace.limit, 6);
-      assert.strictEqual(this.oldSpace, space);
-      assert.strictEqual(this.causedBy, 'root#set');
+  describe('function updating', function() {
+    describe('key setter', function() {
+      it('updates single keys with a value', function() {
+        var space = this.space;
+        assert.strictEqual(typeof space('limit'), 'function');
+        space('limit')(6);
+        // existing space doesn't change
+        assert.strictEqual(space, this.space);
+        // correct values given to subscribers
+        assert.notStrictEqual(this.newSpace, space);
+        assert.strictEqual(this.newSpace.limit, 6);
+        assert.strictEqual(this.oldSpace, space);
+        assert.strictEqual(this.causedBy, '#set:limit');
+      });
+
+      it('adds single keys with a value', function() {
+        var space = this.space;
+        assert.strictEqual(space.newVal, undefined);
+        space('newVal')(true);
+        // existing space doesn't change
+        assert.strictEqual(space, this.space);
+        // correct values given to subscribers
+        assert.notStrictEqual(this.newSpace, space);
+        assert.strictEqual(this.newSpace.newVal, true);
+        assert.strictEqual(this.oldSpace, space);
+        assert.strictEqual(this.causedBy, '#set:newVal');
+      });
+    });
+
+    describe('actions', function() {
+      beforeEach(function() {
+        this.incLimit = function incLimit({ space }) {
+          return { limit: space.limit + 1 };
+        };
+        this.setLimit = ({ value: limit }) => ({ limit });
+      });
+      it('updates the space', function() {
+        var space = this.space;
+        space(this.incLimit)();
+        // existing space doesn't change
+        assert.strictEqual(space, this.space);
+        assert.strictEqual(space.limit, 5);
+        // correct values given to subscribers
+        assert.notStrictEqual(this.newSpace, space);
+        assert.strictEqual(this.newSpace.limit, 6);
+        assert.strictEqual(this.oldSpace, space);
+        assert.strictEqual(this.causedBy, '#incLimit');
+      });
+
+      it('passes in a value', function() {
+        this.space(this.setLimit)(10);
+        assert.strictEqual(this.newSpace.limit, 10);
+      });
+
+      it('supports actions with no name', function() {
+        this.space(this.setLimit)(10);
+        assert.strictEqual(this.causedBy, '#unknown');
+      });
+
+      it('guesses actions names correctly', function() {
+        var decLimit = ({ space }) => ({ limit: space.limit - 1 });
+        this.space(decLimit)();
+        assert.strictEqual(this.causedBy, '#decLimit');
+      });
+
+      it('supports explicit action names', function() {
+        this.space(this.setLimit, 'setLimit')(10);
+        assert.strictEqual(this.causedBy, '#setLimit');
+      });
     });
   });
 });
